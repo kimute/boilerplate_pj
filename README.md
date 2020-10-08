0e8a240f09f0d4bac1360b06776d8dd229093bcb
kimute <jkdnekki@icloud.com>
2020-10-08 16:14:53 +0900

    modify mongo config

---
+++ README.md
@@ -0,0 +1,24 @@
+# Boilerplate
+
+## 背景:
+
+ウェブアプリを作成すると LOGIN 関する認証機能が必要となる
+毎回０から作成するより LOGIN 認証に関する機能は Boilerplate して
+再利用することでコードを書く時間が短縮できる
+
+# required
+
+- NodeJS(v13.8.0)
+  - Express
+  - Body-parser
+  - Cookie-parser
+  - Nodemon
+- MongoDB
+
+  - mongoose
+
+  ## Fuctions
+
+  - [x] Login
+  - [x] Logout
+  - [x] Authentication
--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const config = require("./config/key");
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
+
+//clientから来る情報をサーバーで分析しもらえる処理
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+//application/json
+app.use(bodyParser.json());
+
 const mongoose = require("mongoose");
 mongoose
   .connect(config.mongoURI, {
@@ -13,6 +22,21 @@ mongoose
   .then(() => console.log("MongoDB Connected...."))
   .catch((err) => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello world"));
+app.get("/", (req, res) => res.send("Hello world??"));
+
+//会員登録のためのrouterを作成
+//endpointはresgisterにする
+app.post("/register", (req, res) => {
+  // 会員情報をclientからget　→　データーベースに入れる
+  //body-parserによりjson形式のデーターがDBに入られる
+  const user = new User(req.body);
+
+  //以下はmongoDBで使うmethode
+  //userモデルにsave
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({ success: true });
+  });
+});
 
 app.listen(port, () => console.log(`Express app listening port ${port}`));
